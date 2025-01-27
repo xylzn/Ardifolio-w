04b5bc8ce0b29d46a4fd59880e14031a2c8eb1c8
Dedi Rialdi As Aldi <143459592+xylzn@users.noreply.github.com>
2025-01-27 14:35:09 +0700

    tah ajg

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,33 @@ function getYear() {
 getYear();
 
 
+    window.onscroll = function() {
+        const header = document.querySelector('.header_section');
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            header.classList.add('header_fixed');
+        } else {
+            header.classList.remove('header_fixed');
+        }
+    };
+
+ 
+
+    document.querySelectorAll('.navbar-nav a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault(); // Mencegah perilaku default anchor
+
+            const targetId = this.getAttribute('href'); // Mendapatkan ID target
+            const targetElement = document.querySelector(targetId); // Menemukan elemen target
+
+            if (targetElement) { // Pastikan elemen target ada
+                targetElement.scrollIntoView({ behavior: 'smooth' }); // Scroll ke elemen target
+            } else {
+                console.error(`Element with ID ${targetId} not found.`);
+            }
+        });
+    });
+
+
 // client section owl carousel
 $(".client_owl-carousel").owlCarousel({
     loop: true,
